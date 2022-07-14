56c9041d82377689599d5f3cb8eb26cf7c442187
Subash A <subash@SUI-C0044-X0.local>
2022-07-14 15:01:13 +0530

    Introduce behaviour to fetch rupee and paise

--- pkg/money.go
+++ pkg/money.go
@@ -8,3 +8,11 @@ type Money struct {
 func NewMoney() Money {
 	return Money{}
 }
+
+func (money Money) GetRupee() int64 {
+	return money.rupee
+}
+
+func (money Money) GetPaise() int64 {
+	return money.paise
+}
--- pkg/money_test.go
+++ pkg/money_test.go
@@ -10,3 +10,15 @@ func TestNewMoney(t *testing.T) {
 		assert.IsType(t, Money{}, NewMoney())
 	})
 }
+
+func TestGetRupee(t *testing.T) {
+	t.Run("should return the rupees", func(t *testing.T) {
+		assert.Equal(t, int64(0), NewMoney().GetRupee())
+	})
+}
+
+func TestGetPaise(t *testing.T) {
+	t.Run("should return the paise", func(t *testing.T) {
+		assert.Equal(t, int64(0), NewMoney().GetPaise())
+	})
+}
