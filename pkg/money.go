5cf99fc1fa95b9d98c312346ffbf7ba74fcc9af5
Subash A <subash@SUI-C0044-X0.local>
2022-07-14 15:09:55 +0530

    Introduce behavior to add rupee

--- pkg/money.go
+++ pkg/money.go
@@ -1,18 +1,22 @@
 package main
 
 type Money struct {
-	rupee int64
-	paise int64
+	rupee int
+	paise int
 }
 
 func NewMoney() Money {
 	return Money{}
 }
 
-func (money Money) GetRupee() int64 {
+func (money Money) GetRupee() int {
 	return money.rupee
 }
 
-func (money Money) GetPaise() int64 {
+func (money Money) GetPaise() int {
 	return money.paise
+}
+
+func (money *Money) AddRupee(rupee int) {
+	money.rupee += rupee
 }
--- pkg/money_test.go
+++ pkg/money_test.go
@@ -13,12 +13,22 @@ func TestNewMoney(t *testing.T) {
 
 func TestGetRupee(t *testing.T) {
 	t.Run("should return the rupees", func(t *testing.T) {
-		assert.Equal(t, int64(0), NewMoney().GetRupee())
+		assert.Equal(t, 0, NewMoney().GetRupee())
 	})
 }
 
 func TestGetPaise(t *testing.T) {
 	t.Run("should return the paise", func(t *testing.T) {
-		assert.Equal(t, int64(0), NewMoney().GetPaise())
+		assert.Equal(t, 0, NewMoney().GetPaise())
+	})
+}
+
+func TestAddRupee(t *testing.T) {
+	t.Run("should add rupee to the existing money", func(t *testing.T) {
+		money := NewMoney()
+		money.AddRupee(10)
+		assert.Equal(t, 10, money.GetRupee())
+		money.AddRupee(5)
+		assert.Equal(t, 15, money.GetRupee())
 	})
 }
