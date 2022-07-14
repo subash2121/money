e0c97a1c872c188c41f77634323208a0b92b84ca
Subash A <subash@SUI-C0044-X0.local>
2022-07-14 14:54:35 +0530

    Introduce money entity

--- pkg/money.go
+++ pkg/money.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+type Money struct {
+	rupee int64
+	paise int64
+}
 
+func NewMoney() Money {
+	return Money{}
 }
--- pkg/money_test.go
+++ pkg/money_test.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewMoney(t *testing.T) {
+	t.Run("should be able to initialize and return money entity", func(t *testing.T) {
+		assert.IsType(t, Money{}, NewMoney())
+	})
+}
